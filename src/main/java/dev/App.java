a136c4b7ab11fd6e794eb619fa9edbc47c9ec0a5
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-01-16 13:56:46 +0100

    tp1 spring

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -8,26 +8,20 @@ import dev.service.PlatServiceVersion2;
 
 import java.util.Scanner;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        PlatDaoMemoire platDaoMemoire = new PlatDaoMemoire();
-
-        // TODO adapter le chemin du fichier (utiliser un répertoire existant)
-        PlatDaoFichier platDaoFichier = new PlatDaoFichier("/home/rossi/Temp/restaurant.txt");
-
-        PlatServiceVersion1 platServiceVersion1 = new PlatServiceVersion1(platDaoFichier);
-        PlatServiceVersion2 platServiceVersion2 = new PlatServiceVersion2(platDaoMemoire);
-
-        Menu menu = new Menu(scanner, platServiceVersion1);
-
-        menu.afficher();
-
-        scanner.close();
-
-
-    }
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext
+				("application-config.xml");
+				// récupération du bean Menu
+				Menu menu = context.getBean(Menu.class);
+				menu.afficher();
+				// fermeture du Scanner
+				context.getBean(Scanner.class).close();
+				// fermeture du contexte Spring
+				context.close();
+	}
 }
